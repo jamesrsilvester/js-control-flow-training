4fc8b1ee114eadcdd12b218e37f6f03c15dcdb22
jamesrsilvester <jamesrsilvester@gmail.com>
2017-06-29 08:54:59 -0700

    completed login assignment

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,15 @@
 console.log("login.js loaded");
+
+// 1. Created Login Credentials
+let userLogin = {
+  username: "james",
+  password: "secretword"
+};
+
+// 2. Prompt custom message for user, ask for their password
+// 3.
+var passwordProvided = prompt(`Enter password for ${userLogin.username}`);
+if (passwordProvided !== userLogin.password) {
+  alert ("Incorrect password");}
+  else {console.log("Password Accepted.");
+  }
